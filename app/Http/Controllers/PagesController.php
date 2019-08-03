fd09bea3fe22e1fd0e9f2314f7bbce6d2a08710d
vagrant <vagrant@homestead>
2019-08-03 05:31:43 +0000

    权限控制

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class PagesController extends Controller
 {
-     public function root()
+     public function root(User $user)
     {
-        return view('pages.root');
+        return view('pages.root',compact('user'));
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
-{
+{   
+    
     /**
      * Bootstrap any application services.
      *
