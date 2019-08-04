efcfb3d74d7d080ab6c4421a05570da75e5e7b74
vagrant <vagrant@homestead>
2019-08-04 06:08:24 +0000

    话题列表排序

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Topic;
+use App\Models\User;
 
 class Category extends Model
 {
      protected $fillable = [
             'name','discription',
      ];
+     public function topic()
+     {
+     	return $this->hasMany(Topic::class);
+     }
 }
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -14,4 +14,27 @@ class Topic extends Model
     {
     	return $this->belongsTo(User::class);
     }
+
+    public function scopeRecent($query)
+    {
+        //按照创建时间排序
+        return $query->orderBy('created_at','desc');
+    }
+    public function scopeReplied($query)
+    {
+    	//按照最新回复排序
+        return $query->orderBy('updated_at','desc');
+    }
+    public function scopeWithOrder($query,$order)
+    {
+         //根据order变量读取使用排序
+    	switch($order){
+                 case 'recent':
+                  $query->recent();
+                  break;
+                 default:
+                  $query->replied();
+                  break; 
+    	}
+    }
 }
