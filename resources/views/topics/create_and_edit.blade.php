9ca62a87489722927ddbfbeeae65e456c288abfe
vagrant <vagrant@homestead>
2019-08-04 11:55:56 +0000

    编辑帖子及授权

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -40,7 +40,8 @@
                         <select class="form-control" name="category_id" required>
                             <option value="" hidden disabled selected>请选择分类</option>
                             @foreach ($categories as $value)
-                                <option value="{{ $value->id }}">{{ $value->name }}</option>
+                                <option value="{{ $value->id }}" 
+                                    @if($topic->category_id == $value->id) selected="selected"> @endif {{ $value->name }}</option>
                             @endforeach
                         </select>
                     </div>
