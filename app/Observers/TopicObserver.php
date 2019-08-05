82efc1aa9e89f85a48ac87734d5f6eb6dc0a2a8c
vagrant <vagrant@homestead>
2019-08-05 09:36:59 +0000

    队列的使用以及模型观察器

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Topic;
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -27,12 +28,21 @@ class TopicObserver
     	$topic->excerpt = make_excerpt($topic->body);
 
         //如果slug字段无内容，即使用翻译器对 title进行翻译
-        if(! $topic->slug)
-        {    //1.可以实例化调用
+       // if(! $topic->slug)
+        //{    //1.可以实例化调用
             /*$slurtran = new SlugTranslateHandler;
             $topic->slug = $slurtran->translate($topic->title);*/
             //2.使用laravel的服务容器 app() 允许我们使用 Laravel 服务容器 
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            /*$topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);*/
+            //推送任务到队列
+            //dispatch(new TranslateSlug($topic));
+       // }
+    }
+    public function saved(Topic $topic)
+    {
+        //如slug字段无内容，就使用百度翻译接口对title进行翻译
+        if(! $topic->slug){
+             dispatch(new TranslateSlug($topic));
         }
     }
 }
