97668aba37d3f44dc6073da16003d636ff19c821
vagrant <vagrant@homestead>
2019-08-08 07:25:56 +0000

    多角色用户的权限扩展的安装与配置和使用

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,12 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Models\Topic;
 use App\Models\Reply;
+use Spatie\Permission\Traits\HasRoles;
 use Auth;
 
 class User extends Authenticatable
-{
+{   
+    use HasRoles; 
     use Notifiable{
         notify as protected laravelNotify;
     }
